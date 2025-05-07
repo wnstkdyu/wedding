305e15a1b55c36d74e7744a576d11da858470258
wnstkdyu <wnstkdyu@gmail.com>
2025-05-07 16:10:54 +0900

    og 태그 수정중

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `weddingInvitation`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-react-helmet`,
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { Layout } from "antd";
 import styled from "styled-components";
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -45,28 +46,38 @@ const IndexPage = () => {
     });
   });
   return (
-    <Wrapper>
-      <audio autoPlay loop>
-        <source src={Song} />
-      </audio>
-      <Title />
-      <Greeting />
-      <Gallery />
-      <Location />
-      <Quote />
-      <CongratulatoryMoney />
-      <Share />
-      <Footer
-        style={{
-          background: "#D7CCC8",
-          backgroundImage: `url(${GroovePaper})`,
-          opacity: 0.6,
-          textAlign: "center",
-        }}
-      >
-        Copyright © 2022 Shin Jooyoung
-      </Footer>
-    </Wrapper>
+    <>
+      <Helmet>
+        {/* 기본 필수 */}
+        <meta property="og:type"        content="website" />
+        <meta property="og:title"       content="준상 & 정현 모바일 청첩장" />
+        <meta property="og:description" content="2025년 9월 21일(일) 11:30, 서울동부지방법원, 3층 동백홀" />
+        <meta property="og:image"       content="https://wnstkdyu.github.io/wedding/src/assets/og.JPG" />
+        <meta property="og:url"         content="https://junsang-jeonghyun-wedding.netlify.app/" />
+      </Helmet>
+      <Wrapper>
+        <audio autoPlay loop>
+          <source src={Song} />
+        </audio>
+        <Title />
+        <Greeting />
+        <Gallery />
+        <Location />
+        <Quote />
+        <CongratulatoryMoney />
+        <Share />
+        <Footer
+          style={{
+            background: "#D7CCC8",
+            backgroundImage: `url(${GroovePaper})`,
+            opacity: 0.6,
+            textAlign: "center",
+          }}
+        >
+          Copyright © 2022 Shin Jooyoung
+        </Footer>
+      </Wrapper>
+    </>
   );
 };
 
